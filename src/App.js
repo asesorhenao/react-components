070c1e57ae408c7e319c10e6fe370dd07036e611
xumak <xumak@Jorges-MacBook-Pro.local>
2019-10-30 16:09:03 -0500

    Components and Properties

--- src/App.js
+++ src/App.js
@@ -6,10 +6,56 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <User user={user} />
+        <UsersList list={persons} />
       </header>
     </div>
   );
-}
+};
 
+const Avatar = props => <img src={props.user.img} alt={props.user.name} />;
+const UserName = props => <p>{props.user.name}</p>;
+
+const User = props => {
+  return (
+    <div>
+      <Avatar user= {props.user} />
+      <UserName user = {props.user} />
+    </div>
+  )
+};
+
+const UsersList = props => {
+  const userlist =props.list.map((user,i) => <User user={user} key={i} />);
+  return (
+    <div className="user-list">
+      {userlist}
+    </div>
+  )
+};
+
+const user = {
+  name: 'Alexander Henao',
+  img: logo
+};
+
+
+const persons = [
+  {
+    name: 'Carlos',
+    img: 'https://picsum.photos/100/100'
+  },
+  {
+    name: 'Pedro',
+    img: 'https://picsum.photos/100/100'
+  },
+  {
+    name: 'Sergio',
+    img: 'https://picsum.photos/100/100'
+  },
+  {
+    name: 'David',
+    img: 'https://picsum.photos/100/100'
+  }
+]
 export default App;
